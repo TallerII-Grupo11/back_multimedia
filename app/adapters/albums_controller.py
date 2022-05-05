ed4531dc1061fd6b9ec5c6a244dced4b5ecd38ac
Elianagam <elianagam2@gmail.com>
2022-05-04 23:03:04 -0300

    Fix index

--- app/adapters/albums_controller.py
+++ app/adapters/albums_controller.py
@@ -9,14 +9,15 @@ from app.db.model.album import AlbumModel, UpdateAlbumModel
 router = APIRouter(tags=["albums"])
 
 
-@router.post("/albums",
+@router.post(
+    "/albums",
     response_description="Add new album",
     response_model=AlbumModel,
-    )
+)
 async def create_album(
     album: AlbumModel = Body(...), 
     db: DatabaseManager = Depends(get_database)
-    ):
+):
     created_album = await db.add_album(album)
     return JSONResponse(status_code=status.HTTP_201_CREATED, content=created_album)
 
@@ -26,7 +27,7 @@ async def create_album(
     response_description="List all albums",
     response_model=List[AlbumModel],
     status_code=status.HTTP_200_OK,
-    )
+)
 async def list_albums(db: DatabaseManager = Depends(get_database)):
     albums = await db.get_albums()
     return albums
@@ -37,7 +38,7 @@ async def list_albums(db: DatabaseManager = Depends(get_database)):
     response_description="Get a single album",
     response_model=AlbumModel,
     status_code=status.HTTP_200_OK,
-    )
+)
 async def show_album(id: str, db: DatabaseManager = Depends(get_database)):
     album = await db.get_album(album_id=id)
     if album is not None: 
@@ -46,7 +47,8 @@ async def show_album(id: str, db: DatabaseManager = Depends(get_database)):
     raise HTTPException(status_code=404, detail=f"Album {id} not found")
 
 
-@router.put("/albums/{id}",
+@router.put(
+    "/albums/{id}",
     response_description="Update a album",
     response_model=AlbumModel,
     status_code=status.HTTP_200_OK,
@@ -55,11 +57,12 @@ async def update_album(id: str, album: UpdateAlbumModel = Body(...), db: Databas
     album = await db.update_album(album_id=id, album=album)
     return album
 
-@router.delete("/albums/{id}",
+@router.delete(
+    "/albums/{id}",
     response_description="Delete a album",
     include_in_schema=False,
     status_code=status.HTTP_200_OK,
-    )
+)
 async def delete_album(id: str, db: DatabaseManager = Depends(get_database)):
     delete_result = await db.delete_album(album_id=id)
 
--- app/adapters/songs_controller.py
+++ app/adapters/songs_controller.py
@@ -7,13 +7,14 @@ from app.db.model.song import SongModel, UpdateSongModel
 router = APIRouter(tags=["songs"])
 
 
-@router.post("/songs",
+@router.post(
+    "/songs",
     response_description="Add new song",
     response_model=SongModel
-    )
+)
 async def create_song(
     song: SongModel = Body(...), db: DatabaseManager = Depends(get_database)
-    ):
+):
     created_song = await db.add_song(song)
     return JSONResponse(status_code=status.HTTP_201_CREATED, content=created_song)
 
@@ -22,7 +23,7 @@ async def create_song(
     response_description="List all songs",
     response_model=List[SongModel],
     status_code=status.HTTP_200_OK,
-    )
+)
 async def list_songs(db: DatabaseManager = Depends(get_database)):
     songs = await db.get_songs()
     return songs
@@ -32,7 +33,7 @@ async def list_songs(db: DatabaseManager = Depends(get_database)):
     response_description="Get a single song",
     response_model=SongModel,
     status_code=status.HTTP_200_OK,
-    )
+)
 async def show_song(id: str, db: DatabaseManager = Depends(get_database)):
     song = await db.get_song(song_id=id)
     if song is not None: 
@@ -41,22 +42,26 @@ async def show_song(id: str, db: DatabaseManager = Depends(get_database)):
     raise HTTPException(status_code=404, detail=f"Song {id} not found")
 
 
-@router.put("/songs/{id}",
+@router.put(
+    "/songs/{id}",
     response_description="Update a song",
     response_model=SongModel,
     status_code=status.HTTP_200_OK,
-    )
-async def update_song(id: str, song: UpdateSongModel = Body(...), 
+)
+async def update_song(
+    id: str, 
+    song: UpdateSongModel = Body(...), 
     db: DatabaseManager = Depends(get_database)
-    ):
+):
     song = await db.update_song(song_id=id, song=song)
     return song
 
-@router.delete("/songs/{id}",
+@router.delete(
+    "/songs/{id}",
     response_description="Delete a song",
     include_in_schema=False,
     status_code=status.HTTP_200_OK,
-    )
+)
 async def delete_song(id: str, db: DatabaseManager = Depends(get_database)):
     delete_result = await db.delete_song(song_id=id)
 
