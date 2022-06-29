fb313ca5a04a71d4b1b5c635b1d08da04f018d7e
Elianagam <elianagam2@gmail.com>
2022-06-29 20:26:16 -0300

    Fix linter

--- app/rest/metric_client.py
+++ app/rest/metric_client.py
@@ -2,7 +2,6 @@ import httpx
 import json
 
 from fastapi import Body
-from typing import List
 from app.db.model.song import SongModel
 from app.db.model.album import AlbumModel
 from app.db.model.playlist import PlaylistModel
@@ -15,12 +14,12 @@ class MetricClient:
 
     def post_new_song(self, song: SongModel = Body(...)):
         song = jsonable_encoder(song)
-        r = httpx.post(f'{self.api_url}/song', data=json.dumps(song))
+        httpx.post(f'{self.api_url}/song', data=json.dumps(song))
 
     def post_new_album(self, album: AlbumModel = Body(...)):
         album = jsonable_encoder(album)
-        r = httpx.post(f'{self.api_url}/album', data=json.dumps(album))
+        httpx.post(f'{self.api_url}/album', data=json.dumps(album))
 
     def post_new_playlist(self, playlist: PlaylistModel = Body(...)):
         playlist = jsonable_encoder(playlist)
-        r = httpx.post(f'{self.api_url}/playlist', data=json.dumps(playlist))
+        httpx.post(f'{self.api_url}/playlist', data=json.dumps(playlist))
